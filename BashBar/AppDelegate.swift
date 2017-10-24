6bab84c18427fff8e4820b3b076c5e1e19991d91
Tom Brek <tbrek@globalpersonals.co.uk>
2017-10-24 15:48:10 +0100

    Initial Commit

---
+++ BashBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BashBar
+//
+//  Created by Tom Brek on 24/10/2017.
+//  Copyright © 2017 Tom Brek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
