385ea75d934faba0a1da66eb9d441335edbb00dd
tbrek <me@tombrek.com>
2017-10-25 23:16:47 +0100

    Adding table

--- BashBar/AppDelegate.swift
+++ BashBar/AppDelegate.swift
@@ -10,11 +10,18 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
+    
+    @IBOutlet weak var preferencesWindow: NSWindow!
+    @IBOutlet weak var menuTable: NSTableView!
+    
+    var menu1Data = ["A"]
+    var menu2Data = ["1"]
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        menuTable.beginUpdates()
+        menuTable.insertRows(at: [IndexPath(row:1, section: 0)], withAnimation: .automatic)
+        menuTable.endUpdates()
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ BashBar/PreferencesController.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesController.swift
+//  BashBar
+//
+//  Created by Tom Brek on 25/10/2017.
+//  Copyright © 2017 Tom Brek. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
