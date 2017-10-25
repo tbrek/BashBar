ad2bc68705d842d7ee0a3769d008a8267c84e40a
tbrek <me@tombrek.com>
2017-10-25 22:46:46 +0100

    Adding icon Adding menu

---
+++ BashBar/StatusMenuController.swift
@@ -0,0 +1,33 @@
+//
+//  StatusMenuController.swift
+//  BashBar
+//
+//  Created by Tom Brek on 25/10/2017.
+//  Copyright © 2017 Tom Brek. All rights reserved.
+//
+
+import Cocoa
+
+class StatusMenuController: NSObject {
+    
+    @IBOutlet weak var bashMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    override func awakeFromNib() {
+        //        statusItem.title = "bashBar"
+        //        statusItem.menu = bashMenu
+        let icon = NSImage(named: NSImage.Name(rawValue: "statusIcon"))
+        icon?.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = bashMenu
+        
+        // Insert code here to initialize your application
+    }
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    
+}
