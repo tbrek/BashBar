30cc573110233c64ac9f9431c17300141948a1f3
Tom Brek <tbrek@globalpersonals.co.uk>
2017-10-26 16:01:55 +0100

    More changes

--- BashBar/AppDelegate.swift
+++ BashBar/AppDelegate.swift
@@ -12,16 +12,14 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var preferencesWindow: NSWindow!
-    @IBOutlet weak var menuTable: NSTableView!
+
     
     var menu1Data = ["A"]
     var menu2Data = ["1"]
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        menuTable.beginUpdates()
-        menuTable.insertRows(at: [IndexPath(row:1, section: 0)], withAnimation: .automatic)
-        menuTable.endUpdates()
+   
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
--- BashBar/PreferencesController.swift
+++ BashBar/PreferencesController.swift
@@ -10,6 +10,23 @@ import Cocoa
 
 class PreferencesController: NSWindowController {
 
+    @IBOutlet weak var sub1: NSButton!
+    
+    @IBOutlet weak var label1_0: NSTextField!
+    @IBOutlet weak var cmd1_0: NSTextField!
+    @IBOutlet weak var label1_1: NSTextField!
+    @IBOutlet weak var cmd1_1: NSTextField!
+    @IBOutlet weak var label1_2: NSTextField!
+    @IBOutlet weak var cmd1_2: NSTextField!
+    @IBOutlet weak var label1_3: NSTextField!
+    @IBOutlet weak var cmd1_3: NSTextField!
+    @IBOutlet weak var subCheck1: NSButton!
+    
+    @IBAction func subMenuCheck1(_ sender: Any) {
+        label1_1.isEnabled = Bool(subCheck1.state)
+        
+    }
+   
     override func windowDidLoad() {
         super.windowDidLoad()
     
